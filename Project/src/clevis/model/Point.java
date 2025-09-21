ed22f348ff9874d02ee610cf55ed8b5a2b2d7b77
MiaoYu <MiaoYu404@icloud.com>
2025-09-21 22:30:53 +0800

    fulfill some unfinished function, like dot(), det(), distance();
    fixed wrong include() function and make it "onSegment()".
    
    it now can tell whether a point is inside any Shape (Convex polygon and Circle only).

--- Project/src/clevis/model/Point.java
+++ Project/src/clevis/model/Point.java
@@ -1,6 +1,7 @@
 package clevis.model;
 
 import static clevis.model.ComputingGeometry.eps;
+import static clevis.model.ComputingGeometry.sign;
 
 public class Point {
     double x, y;
@@ -37,17 +38,44 @@ public class Point {
     }
 
     // dot product
-    double dot(Point p) { return 0.0; }
+    double dot(Point p) { return x * p.x + y * p.y; }
 
     // det product
-    double det(Point p) { return 0.0; }
+    double det(Point p) { return x * p.y - y * p.x; }
 
     // return the distance from this point to point p;
-    double distance(Point p) { return 0.0; }
+    double distance(Point p) { return Points.minus(this, p).abs(); }
 
     // abs from origin
     double abs() { return Math.sqrt(abs2()); }
     double abs2() { return x * x + y * y; }
+
+    boolean onSegment(Line l) {
+        Point p1 = l.points[0], p2 = l.points[1];
+        return sign(Points.minus(p1, this).det(Points.minus(p2, this))) == 0
+                && sign((p1.x - this.x) * (p2.x - this.x)) <= 0
+                && sign((p1.y - this.y) * (p2.y - this.y)) <= 0;
+    }
+
+    boolean inside(Shape s) {
+        if (s instanceof Circle c) {
+            return sign(Points.minus(c.points[0], this).abs() - c.r) <= 0;
+        }
+        double angle = 0;
+        Point p1, p2; Point v1, v2;
+        for (int i = 0, j = s.points.length - 1; i < s.points.length; j = i++) {
+            p1 = s.points[i]; p2 = s.points[j];
+            if (onSegment(new Line(p1, p2))) return true;
+
+            v1 = Points.minus(p1, this);
+            v2 = Points.minus(p2, this);
+            double res = Math.atan2(v2.y, v2.x) - Math.atan2(v1.y, v1.x);
+            res = Math.abs(res);
+            if (sign(res - Math.PI) > 0) res = 2 * Math.PI - res;
+            angle += res;
+        }
+        return sign(2 * Math.PI - angle) == 0;
+    }
 
     @Override
     public String toString() {
