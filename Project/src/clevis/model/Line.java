5ffb7ae1e439c92cde4070e10df388de7b657e8a
MiaoYu <MiaoYu404@icloud.com>
2025-09-21 16:34:00 +0800

    modify Line.java - make the Line extends Shape class.   - combine ps[] array and points[](from superclass Shape) to make it clear; - modify var name "p1" to be "from", "p2" to be "to" to show the relationship, and also the direction. - [override] toString(), printInfo() is done. - Construct with a name is done.

--- Project/src/clevis/model/Line.java
+++ Project/src/clevis/model/Line.java
@@ -1,24 +1,42 @@
 package clevis.model;
 
-import static clevis.model.ComputingGeometry.eps;
 import static clevis.model.ComputingGeometry.sign;
 
-public class Line {
-    Point[] ps = new Point[2];
+public class Line extends Shape{
 
     Line() { }
-    Line(Point p1, Point p2) {
-        ps[0] = p1;
-        ps[1] = p2;
+    Line(Point from, Point to) {
+        points = new Point[]{from, to};
     }
+    Line(String _name, Point from, Point to) { this(from, to); name = _name; }
+    Line(Line o) { this(o.name, o.points[0], o.points[1]); }
 
     // return the direction of the vector
-    Point direction() { return Points.minus(ps[1], ps[0]); }
+    Point direction() { return Points.minus(points[1], points[0]); }
 
     // point is on the line ?
     boolean include (Point p) {
         return sign(direction().det(
-                Points.minus(p, ps[0])
+                Points.minus(p, points[0])
         )) > 0;
+    }
+
+    @Override
+    public String toString() {
+        return points[0].toString() + " -> " + points[1].toString();
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println(this);
+    }
+
+    @Override
+    public Rectangle boundingBox() {
+        Point topLeft = new Point(Math.min(points[0].x, points[1].x), Math.max(points[0].y, points[1].y));
+        double width = Math.abs(points[0].x - points[1].x);
+        double height = Math.abs(points[0].y - points[1].y);
+        String bbName = "Bounding Box of '" + name + "'";
+        return new Rectangle(bbName, topLeft, width, height);
     }
 }
