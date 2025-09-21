d92b43ad89d7c0bd59074b5343c5f52295843907
MiaoYu <MiaoYu404@icloud.com>
2025-09-21 22:32:46 +0800

    Tests of points are inside some shape is provided.

--- Project/test/clevis/model/TestPoint.java
+++ Project/test/clevis/model/TestPoint.java
@@ -31,4 +31,17 @@ public class TestPoint {
         System.out.println("Calculation result is x: " + rsl.x + ", y: " + rsl.y + ".");
         assertThat(Points.divide(p1, 2)).isEqualTo(new Point(-0.49999999999, 0.0));
     }
+
+    @Test
+    public void testInside() {
+        Point tlp = new Point(-5, 3);
+        Rectangle r = new Rectangle(tlp, 9.5, 3.5);
+
+        Point p1 = new Point(-5, 1);
+        Point p2 = new Point(Math.PI, Math.PI);
+        Point p3 = new Point(Math.E, Math.E);
+        assertThat(p1.inside(r)).isTrue();
+        assertThat(p2.inside(r)).isFalse();
+        assertThat(p3.inside(r)).isTrue();
+    }
 }
