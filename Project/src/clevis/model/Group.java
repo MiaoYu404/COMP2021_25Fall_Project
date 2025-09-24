85aa9fe5ac6b460acf335df6dab81782992df78c
MiaoYu <MiaoYu404@icloud.com>
2025-09-24 16:40:01 +0800

    father method(s) added, construct method added.

--- Project/src/clevis/model/Group.java
+++ Project/src/clevis/model/Group.java
@@ -2,12 +2,52 @@ package clevis.model;
 
 import org.w3c.dom.css.Rect;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * class for Group
  */
 public class Group implements Shape{
     private String name;
-    private Shape[] shapes;
+    private List<Shape> shapes;
+
+    private Shape father;
+
+    /**
+     * @param _name name
+     * @param _shapes shapes
+     */
+    public Group(String _name, ArrayList<Shape> _shapes) {
+        name = _name;
+        shapes = new ArrayList<Shape>(_shapes);
+    }
+
+    /**
+     * @return copy of shapes;
+     */
+    public List<Shape> shapes() {
+        return shapes;
+    }
+
+    @Override
+    public String name() { return name; }
+
+    @Override
+    public Shape getFather() {
+        return father;
+    }
+
+    @Override
+    public void setFather(Shape _shape) {
+        father = _shape;
+    }
+
+    @Override
+    public boolean haveFather() {
+        return father != null;
+    }
 
     @Override
     public void move(double dx, double dy) {
