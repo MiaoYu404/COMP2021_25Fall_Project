40fa0dade59c291455d6c3e00e4261d803021385
MiaoYu <MiaoYu404@icloud.com>
2025-11-06 15:31:06 +0800

    simple main method

--- Project/src/clevis/Application.java
+++ Project/src/clevis/Application.java
@@ -1,6 +1,10 @@
 package clevis;
 
 import clevis.system.Clevis;
+import clevis.system.Console;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * reading two files
@@ -12,8 +16,23 @@ public class Application {
      */
     public static void main(String[] args){
         Clevis clevis = new Clevis();
+        Console console = new Console();
         // Initialize and utilize the system
-		
+		Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String inp = scanner.nextLine();
+            String[] line = inp.split(" ");
+            switch (line[0]) {
+                case "add":
+                    console.add(Arrays.copyOfRange(line, 1, line.length));
+                    break;
+                case "list":
+                    console.list(line[1]);
+                    break;
+                case "quit":
+                    console.quit();
+            }
+        }
     }
 
 }
