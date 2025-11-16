473ad1216b88e65daff93a77ba4ef3a12ba99353
lcy <24103385d@connect.polyu.hk>
2025-11-16 22:12:46 +0800

    test update

--- Clevis/test/clevis/util/TestCircle.java
+++ Clevis/test/clevis/util/TestCircle.java
@@ -1,16 +1,21 @@
-package clevis.util;
+package clevis.model;
 
-import clevis.util.shape.Circle;
-import clevis.util.shape.Point;
+import clevis.util.Circle;
+import clevis.util.Point;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-/**
- * test Circle
- */
 public class TestCircle {
-    /**
-     * test Circle to String
-     */
+    Circle c1, c2, c3, c4;
+
+    @Before
+    public void setUp() {
+        c1 = new Circle();
+        c2 = new Circle(new Point(1,2),3);
+        c3 = new Circle(new Point(5, 6),1);
+        c4 = new Circle(new Point(1,4),2);
+    }
     @Test
     public void test2String() {
         Point p = new Point(0, 0);
@@ -18,13 +23,47 @@ public class TestCircle {
         System.out.println(circle);
     }
 
-    /**
-     * test bounding box
-     */
     @Test
     public void testBoundingBox() {
         Point p = new Point(0, 0);
         Circle circle = new Circle("bad name", p, 1);
         System.out.println(circle.boundingBox());
     }
+
+    @Test
+    public void testEquals(){
+        assertFalse(c1.equals(c2));
+        assertFalse(c1.equals(c3));
+        assertFalse(c1.equals(c4));
+        assertTrue(c1.equals(c1));
+        assertTrue(c2.equals(c2));
+
+    }
+
+    @Test
+    public void testIntersects(){
+        assertTrue(c1.intersects(c2));
+        assertFalse(c1.intersects(c3));
+        assertFalse(c1.intersects(c4));
+        assertTrue(c1.intersects(c1));
+        assertTrue(c3.intersects(c3));
+    }
+
+    @Test
+    public void testMove(){
+        c2.move(-2,-3);
+        Point newC2 = new Point(-1,-1);
+        assertEquals(c2.center(),newC2);
+
+        c3.move(5,6);
+        Point newC3 = new Point(10,12);
+        assertEquals(c3.center(),newC3);
+
+        c4.move(0,0);
+        Point newC4 = new Point(1,4);
+        assertEquals(c4.center(),newC4);
+    }
+
 }
+
+
--- Clevis/test/clevis/util/TestConsole.java
+++ Clevis/test/clevis/util/TestConsole.java
@@ -1,137 +1,54 @@
-package clevis.util;
+package clevis.model;
 
-import clevis.Application;
 import clevis.system.Console;
-
-import clevis.system.Data;
-import clevis.util.operation.OpAdd;
-import clevis.util.operation.Operation;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 import static com.google.common.truth.Truth.assertThat;
 
-/**
- * test the console
- */
 public class TestConsole {
-    /**
-     * basic test
-     */
     @Test
     public void test() {
-        Application app = new Application();
-        Console console = app.console();
+        Console console = new Console();
 
         String op;
         op = "circle A 1 7 3";
-        console.readOperation(op);
-
-        op = "list A";
-        console.readOperation(op);
+        console.add(op.split(" "));
+        console.list("A");
 
         op = "line B 2 3 5 6";
-        console.readOperation(op);
+        console.add(op.split(" "));
         assertThat(console.intersects("A", "B")).isTrue();
 
         op = "rectangle C 3 10 2 3";
-        console.readOperation(op);
+        console.add(op.split(" "));
         assertThat(console.intersects("B", "C")).isFalse();
-
-        op = "group G1 A C";
-        console.readOperation(op);
+        console.group("G1", List.of("A", "C"));
         assertThat(console.intersects("B", "G1")).isTrue();
         try {
-            op = "move C 1.5 -0.5";
-            console.readOperation(op);
+            console.move("C", 1.5, -0.5);
         } catch (Exception e) {
             // success
             System.out.println("illegal move catched.");
         }
-        op = "ungroup G1";
-        console.readOperation(op);
+        console.ungroup("G1");
         assertThat(console.intersects("B", "C")).isFalse();
 
         op = "line D -2 9 5 9";
-        console.readOperation(op);
+        console.add(op.split(" "));
         assertThat(console.shapeAt(3.2, 9.0).name()).isEqualTo("D");
         assertThat(console.shapeAt(3.2, 8.0).name()).isEqualTo("C");
         assertThat(console.shapeAt(2, 8).name()).isEqualTo("A");
 
-        op = "move B -0.8 0";
-        console.readOperation(op);
+        console.move("B", -0.8, 0);
         assertThat(console.shapeAt(3, 4.8).name()).isEqualTo("B");
-
-        op = "group G2 A C";
-        console.readOperation(op);
+        console.group("G2", List.of("A", "C"));
         assertThat(console.shapeAt(3, 4.8).name()).isEqualTo("G2");
-
-        op = "boundingbox G2";
-        console.readOperation(op);
-
-        op = "move G2 -1 0";
-        console.readOperation(op);
-
-        op = "boundingbox G2";
-        console.readOperation(op);
-
-    }
-
-    /**
-     *  test undo & redo
-     */
-    @Test
-    public void testUndo() {
-        Application app = new Application();
-        Console console = app.console();
-        String op;
-
-        op = "listAll";
-        console.readOperation(op);
-
-        op = "rectangle A -1 1 2 2";
-        console.readOperation(op);
-        op = "circle C 0 0 1";
-        console.readOperation(op);
-
-        op = "listAll";
-        console.readOperation(op);
-
-        op = "undo";
-        console.readOperation(op);
-
-        try {
-            op = "list C";
-            console.readOperation(op);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        };
-
-        op = "redo";
-        console.readOperation(op);
-        op = "list C";
-        console.readOperation(op);
-    }
-
-
-    /**
-     * test for Operation
-     */
-    @Test
-    public void testOperation() {
-        Operation o1 = new Operation();
-        o1.call();
-        o1.print();
-        o1.redo();o1.undo();
-    }
-
-    /**
-     * test for opAdd
-     */
-    @Test
-    public void testOpAdd() {
-        Data dat = new Data();
-        String[] arg = "square Joey 0 0 5".split(" ");
-        OpAdd oa = new OpAdd(arg, dat);
-        assertThat(oa.order()).isEqualTo(0);
-
+        console.getBoundingBox("G2");
+        console.move("G2", -1, 0);
+        console.getBoundingBox("G2");
     }
 }
+
