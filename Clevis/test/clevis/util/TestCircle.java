c21501ee152669db1e7cad63c3f56fecbb528f1f
MiaoYu <MiaoYu404@icloud.com>
2025-11-16 18:12:22 +0800

    test

--- Clevis/test/clevis/util/TestCircle.java
+++ Clevis/test/clevis/util/TestCircle.java
@@ -1,8 +1,16 @@
 package clevis.util;
 
+import clevis.util.shape.Circle;
+import clevis.util.shape.Point;
 import org.junit.Test;
 
+/**
+ * test Circle
+ */
 public class TestCircle {
+    /**
+     * test Circle to String
+     */
     @Test
     public void test2String() {
         Point p = new Point(0, 0);
@@ -10,6 +18,9 @@ public class TestCircle {
         System.out.println(circle);
     }
 
+    /**
+     * test bounding box
+     */
     @Test
     public void testBoundingBox() {
         Point p = new Point(0, 0);
